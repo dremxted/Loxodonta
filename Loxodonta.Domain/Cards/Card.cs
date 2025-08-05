d9a6debd06d4bc821c486aea729d90322b688f34
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-06 00:08:36 +0200

    Add Domain models: Card, Feature

---
+++ Loxodonta.Domain/Cards/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace Loxodonta.Domain.Cards;
+
+public class Card
+{
+    private List<Feature> _features = new();
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public IReadOnlyCollection<Feature> Features => _features;
+
+    public void AddFeature(string name, string value, int order)
+    {
+        _features.Add(new Feature(this, name, value, order));
+    }
+}
---
+++ Loxodonta.Domain/Cards/Feature.cs
@@ -0,0 +1,22 @@
+﻿namespace Loxodonta.Domain.Cards;
+
+public class Feature
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public int Order { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Value { get; private set; } = string.Empty;
+
+    public Guid? CardId { get; private set; }
+    public Card? Card { get; private set; }
+
+    private Feature() { }
+    internal Feature(Card card, string name, string value, int order)
+    {
+        Card = card;
+        CardId = card.Id;
+        Name = name;
+        Value = value;
+        Order = order;
+    }
+}
