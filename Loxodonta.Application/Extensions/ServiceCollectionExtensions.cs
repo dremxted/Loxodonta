4980ec4e34199dd993010c9c2712c258f0710543
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:52:41 +0200

    feat(application): add ServiceCollectionExtensions class
    - add AddApplicationServices method
    - bind CardService to ICardService in DI Container

---
+++ Loxodonta.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Loxodonta.Application.Cards;
+using Loxodonta.Application.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Loxodonta.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICardService, CardService>();
+    }
+}
