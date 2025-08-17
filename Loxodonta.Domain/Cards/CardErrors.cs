0af77fda1942434d77a0af6fc7e8e01f58bc5cdb
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 17:36:13 +0200

    feat(domain): add CardErrors class

---
+++ Loxodonta.Domain/Cards/CardErrors.cs
@@ -0,0 +1,16 @@
+﻿using Loxodonta.Common;
+
+namespace Loxodonta.Domain.Cards;
+
+public static class CardErrors
+{
+    public static Error NotFound(Guid id) =>
+        Error.NotFound(
+        "Cards.Card",
+        $"The card with id '{id}' was not found.");
+    public static Error Failure(Guid id) =>
+        Error.Failure("Cards.Card", $"Operation failed with card '{id}'");
+
+    public static Error Validation(params object[] extensions) =>
+        Error.Validation("Cards.Card", "Validation error has occured.", extensions);
+}
