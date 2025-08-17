fd078959b236c0a015ccab4e7689d60280aac2a4
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:37:26 +0200

    feat(application): add EntityToDtoExtensions class for mapping extension methods
    - add Card ToDto extension method
    - add Feature ToDto extension method

---
+++ Loxodonta.Application/Extensions/Entities/CreateDtoToEntityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Loxodonta.Application.Cards;
+using Loxodonta.Domain.Cards;
+
+namespace Loxodonta.Application.Extensions.Entities;
+
+public static class CreateDtoToEntityExtensions
+{
+    public static Card ToEntity(this CreateCardDto createDto)
+    {
+        var card = new Card();
+        foreach (var feature in createDto.Features)
+        {
+            card.AddFeature(feature.Name,feature.Value,feature.Order);
+        }
+
+        return card;
+    }
+}
---
+++ Loxodonta.Application/Extensions/Entities/EntityToDtoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Loxodonta.Application.Cards;
+using Loxodonta.Application.Features;
+using Loxodonta.Domain.Cards;
+
+namespace Loxodonta.Application.Extensions.Entities;
+
+public static class EntityToDtoExtensions
+{
+    public static CardDto ToDto(this Card card)
+    {
+        return new CardDto()
+        {
+            Id = card.Id,
+            Features = card.Features
+                .Select(f => f.ToDto())
+                .ToList()
+        };
+    }
+
+    public static FeatureDto ToDto(this Feature feature)
+    {
+        return new FeatureDto()
+        {
+            Id = feature.Id,
+            Name = feature.Name,
+            Value = feature.Value,
+            Order = feature.Order
+        };
+    }
+}
