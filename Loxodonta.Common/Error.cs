0337c1fa9f355cd72da218b865aac71e81cb27d7
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 17:34:58 +0200

    feat(common): add Result and Error classes

---
+++ Loxodonta.Common/Error.cs
@@ -0,0 +1,35 @@
+﻿namespace Loxodonta.Common;
+
+public sealed record Error
+{
+    public readonly string Code;
+    public readonly string Message;
+    public readonly ErrorType? Type;
+    private readonly object[] _extensions;
+    public IReadOnlyCollection<object> Extensions => _extensions;
+
+    internal Error(ErrorType? errorType, string code, string message, object[] extensions)
+    {
+        Type = errorType;
+        Code = code;
+        Message = message;
+        _extensions = extensions;
+    }
+
+    public static readonly Error None = new Error(null, string.Empty, string.Empty, []);
+    public static Error NotFound(string code, string description, params object[] extensions) =>
+        new Error(ErrorType.NotFound, code, description, extensions);
+    public static Error Failure(string code, string description, params object[] extensions) =>
+        new Error(ErrorType.Failure, code, description, extensions);
+
+    public static Error Validation(string code, string description, params object[] extensions) =>
+        new Error(ErrorType.Validation, code, description, extensions);
+}
+
+public enum ErrorType
+{
+    Failure = 0,
+    Validation = 1,
+    NotFound = 2,
+    Conflict = 3
+}
---
+++ Loxodonta.Common/Result.cs
@@ -0,0 +1,50 @@
+﻿namespace Loxodonta.Common;
+
+public class Result
+{
+    public readonly bool IsSuccess;
+    public readonly Error Error;
+    internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None ||
+            !isSuccess && error == Error.None)
+        {
+            throw new ArgumentException("Invalid error", nameof(error));
+        }
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result<TValue> Success<TValue>(TValue value)
+    {
+        return new Result<TValue>(true, Error.None, value);
+    }
+
+    public static Result<TValue> Failure<TValue>(Error error)
+    {
+        return new Result<TValue>(false, error, default);
+    }
+
+    public TOut Match<TOut>(Func<TOut> success, Func<Error, TOut> failure)
+    {
+        return IsSuccess ? success() : failure(Error);
+    }
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+
+    internal Result(bool isSuccess, Error error, TValue? value) : base(isSuccess, error)
+    {
+        _value = value;
+    }
+    public TOut Match<TOut>(Func<TValue, TOut> success, Func<Error, TOut> failure)
+    {
+        return IsSuccess ? success(Value) : failure(Error);
+    }
+}
