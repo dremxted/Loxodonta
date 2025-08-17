4f4fb933655a093a16e94c7de9bc87028061db2a
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:47:46 +0200

    feat(application): add CardService class
    -add CreateAsync method

---
+++ Loxodonta.Application/Cards/CardService.cs
@@ -0,0 +1,25 @@
+﻿using Loxodonta.Application.Contracts;
+using Loxodonta.Application.Extensions.Entities;
+using Loxodonta.Common;
+using Loxodonta.Domain.Contracts;
+
+namespace Loxodonta.Application.Cards;
+
+public class CardService(ICardRepository cardRepository) : ICardService
+{
+    public async Task<Result<CardDto>> CreateAsync(CreateCardDto createCardDto)
+    {
+        // Override unexpected order values to avoid gaps between them.
+        createCardDto.Features = createCardDto.Features
+            .OrderBy(f => f.Order)
+            .Select((feature, index) =>
+            {
+                feature.Order = index;
+                return feature;
+            })
+            .ToList();
+
+        var card = await cardRepository.CreateAsync(createCardDto.ToEntity());
+        return Result.Success(card.ToDto());
+    }
+}
