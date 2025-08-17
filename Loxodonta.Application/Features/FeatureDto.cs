de377780626ecb0a4e51a6daf15a485bf7278730
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:13:26 +0200

    feat(application): add Feature DTO models
    -add FeatureDto model
    -add CreateCardFeatureDto model

---
+++ Loxodonta.Application/Features/FeatureDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Loxodonta.Application.Features;
+
+public class FeatureDto
+{
+    public Guid Id { get; set; }
+    public int Order { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+}
