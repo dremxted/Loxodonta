49ef8330d3e5336bdcdcb222ef8f673539ff6553
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:57:10 +0200

    feat(api): add CardController
    - add CreateAsync endpoint

---
+++ Loxodonta.API/Controllers/CardController.cs
@@ -0,0 +1,17 @@
+﻿using Loxodonta.Application.Cards;
+using Loxodonta.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loxodonta.API.Controllers;
+
+[ApiController]
+[Route("api/cards")]
+public class CardController(ICardService cardService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateCardDto createCardDto)
+    {
+        var result = await cardService.CreateAsync(createCardDto);
+        return result.Match<IActionResult>((card) => Created("",result.Value),BadRequest);
+    }
+}
