6102384c059bba55c023492c601edc853c9528e9
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 19:22:35 +0200

    feat(domain): add CardRepository class
    - add CreateAsync method

---
+++ Loxodonta.Infrastructure/Repositories/CardRepository.cs
@@ -0,0 +1,17 @@
+﻿using Loxodonta.Domain.Cards;
+using Loxodonta.Domain.Contracts;
+using Loxodonta.Infrastructure.Persistence;
+
+namespace Loxodonta.Infrastructure.Repositories
+{
+    public class CardRepository(ApplicationDbContext dbContext) : ICardRepository
+    {
+        public async Task<Card> CreateAsync(Card card)
+        {
+            await dbContext.AddAsync(card);
+            await dbContext.SaveChangesAsync();
+
+            return card;
+        }
+    }
+}
