981703255937a99a74012668935e45b6c13a3e62
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-17 20:45:02 +0200

    feat(application): add ICardService contract
    - add CreateAsync method

---
+++ Loxodonta.Application/Contracts/ICardService.cs
@@ -0,0 +1,9 @@
+﻿using Loxodonta.Application.Cards;
+using Loxodonta.Common;
+
+namespace Loxodonta.Application.Contracts;
+
+public interface ICardService
+{
+    Task<Result<CardDto>> CreateAsync(CreateCardDto createCardDto);
+}
