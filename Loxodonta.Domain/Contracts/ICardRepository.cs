50e9e05d3bb89cd247932b5048d9381b0e60bfdd
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-27 22:00:03 +0200

    feat(domain): extend ICardRepository contract
    - add FindAsync method
    - add GetAllAsync method
    - add Delete method
    - add SaveChangesAsync method

--- Loxodonta.Domain/Contracts/ICardRepository.cs
+++ Loxodonta.Domain/Contracts/ICardRepository.cs
@@ -5,4 +5,8 @@ namespace Loxodonta.Domain.Contracts;
 public interface ICardRepository
 {
     Task<Card> CreateAsync(Card card);
+    Task<Card?> FindAsync(Guid id);
+    Task<IEnumerable<Card>> GetAllAsync();
+    void Delete(Card card);
+    Task SaveChangesAsync();
 }
