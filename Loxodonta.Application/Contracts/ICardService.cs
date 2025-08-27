5b2d5baf69a909f4d777f1ea1fab56b7d168c114
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-27 22:39:02 +0200

    feat(applocation): extend ICardService
    - add FindAsync method
    - add GetAllAsync method
    - add UpdateAsync method
    - add DeleteAsync method

--- Loxodonta.Application/Contracts/ICardService.cs
+++ Loxodonta.Application/Contracts/ICardService.cs
@@ -6,4 +6,8 @@ namespace Loxodonta.Application.Contracts;
 public interface ICardService
 {
     Task<Result<CardDto>> CreateAsync(CreateCardDto createCardDto);
+    Task<Result<CardDto>> FindAsync(Guid id);
+    Task<Result<IEnumerable<CardDto>>> GetAllAsync();
+    Task<Result<CardDto>> UpdateAsync(Guid id, UpdateCardDto updateCardDto);
+    Task<Result> DeleteAsync(Guid id);
 }
