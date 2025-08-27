351fd82eceb59473b21ac04c525499d66a5f4c9c
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-27 22:36:20 +0200

    feat(application): add UpdateCardDto

---
+++ Loxodonta.Application/Cards/UpdateCardDto.cs
@@ -0,0 +1,18 @@
+﻿using Loxodonta.Application.Features;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loxodonta.Application.Cards;
+
+public class UpdateCardDto : IValidatableObject
+{
+    public List<UpdateCardFeatureDto> Features { get; set; } = new();
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Features == null)
+        {
+            yield return new ValidationResult("null", [$"{nameof(Features)}"]);
+            yield break;
+        }
+    }
+}
