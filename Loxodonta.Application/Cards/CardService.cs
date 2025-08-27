effebc4e4479d7ae7d0a6fc668d564af62669e70
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-27 22:40:02 +0200

    feat(applocation): extend CardService
    - add FindAsync method
    - add GetAllAsync method
    - add UpdateAsync method
    - add DeleteAsync method

--- Loxodonta.Application/Cards/CardService.cs
+++ Loxodonta.Application/Cards/CardService.cs
@@ -22,4 +22,57 @@ public class CardService(ICardRepository cardRepository) : ICardService
         var card = await cardRepository.CreateAsync(createCardDto.ToEntity());
         return Result.Success(card.ToDto());
     }
+
+    public async Task<Result<CardDto>> FindAsync(Guid id)
+    {
+        var card = await cardRepository.FindAsync(id);
+        if (card == null)
+        {
+            return Result.Failure<CardDto>(Error.NotFound("Cards","Not found."));
+        }
+
+        return Result.Success(card.ToDto());
+    }
+
+    public async Task<Result<IEnumerable<CardDto>>> GetAllAsync()
+    {
+        var allCards = await cardRepository.GetAllAsync();
+        var cardsDto = allCards.Select(card => card.ToDto());
+        return Result.Success(cardsDto);
+    }
+
+    public async Task<Result<CardDto>> UpdateAsync(Guid id, UpdateCardDto updateCardDto)
+    {
+        var card = await cardRepository.FindAsync(id);
+        if (card == null)
+        {
+            return Result.Failure<CardDto>(Error.NotFound("Cards", "Not found."));
+        }
+
+        card.ClearFeatures();
+
+        foreach (var (feature, order) in updateCardDto.Features
+                .OrderBy(feature => feature.Order)
+                .Select((feature, index) => (feature, index)))
+        {
+            card.AddFeature(feature.Name, feature.Value, order);
+        }
+
+        await cardRepository.SaveChangesAsync();
+        return Result.Success(card.ToDto());
+    }
+
+    public async Task<Result> DeleteAsync(Guid id)
+    {
+        var deleteCard = await cardRepository.FindAsync(id);
+        if(deleteCard == null)
+        {
+            return Result.Failure(Error.NotFound("Cards", "Not found."));
+        }
+
+        cardRepository.Delete(deleteCard);
+        await cardRepository.SaveChangesAsync();
+    
+        return Result.Success();
+    }
 }
