8cdca4bb7f1c4e7955ac1e885d038ce544afeb63
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-27 22:29:26 +0200

    feat(infrastructure): extend CardRepository
    - add FindAsync method
    - add GetAllAsync method
    - add Delete method
    - add SaveChangesAsync method

--- Loxodonta.Infrastructure/Repositories/CardRepository.cs
+++ Loxodonta.Infrastructure/Repositories/CardRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Loxodonta.Domain.Cards;
 using Loxodonta.Domain.Contracts;
 using Loxodonta.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Loxodonta.Infrastructure.Repositories
 {
@@ -13,5 +14,30 @@ namespace Loxodonta.Infrastructure.Repositories
 
             return card;
         }
+
+        public async Task<Card?> FindAsync(Guid id)
+        {
+            var card = await dbContext.Cards
+                .Include(c => c.Features)
+                .FirstOrDefaultAsync(card => card.Id == id);
+            return card;
+        }
+        public async Task<IEnumerable<Card>> GetAllAsync()
+        {
+            return await dbContext.Cards
+                .Include(card => card.Features)
+                .ToListAsync();
+        }
+
+        public void Delete(Card card)
+        {
+            dbContext.Cards.Remove(card);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+
     }
 }
