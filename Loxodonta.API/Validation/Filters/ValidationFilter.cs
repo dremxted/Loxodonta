16e2e797e61192f2209345e28171cfb1d7b58966
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-29 23:23:16 +0200

    feat(api): add ValidationFilter of generic model type

---
+++ Loxodonta.API/Validation/Filters/ValidationFilter.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Loxodonta.API.Validation.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Loxodonta.API.Validation.Filters;
+
+public sealed class ValidationFilter<TModel>(
+    IValidator<TModel> validator, 
+    IValidationObjectResultFactory objectResultFactory) 
+    : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        object model = context.ActionArguments
+            .FirstOrDefault(arg => arg.Value is TModel)
+            .Value!;
+
+        var validationResult = await validator.ValidateAsync((TModel)model);
+
+        if (!validationResult.IsValid)
+        {
+            context.Result = objectResultFactory
+                .Create(ToModelStateDictionary(validationResult)); 
+            return;
+        }
+
+        await next();
+    }
+
+    private ModelStateDictionary ToModelStateDictionary(ValidationResult validationResult)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+        foreach (var error in validationResult.Errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.PropertyName,
+                error.ErrorMessage);
+        }
+        return modelStateDictionary;
+    }
+}
