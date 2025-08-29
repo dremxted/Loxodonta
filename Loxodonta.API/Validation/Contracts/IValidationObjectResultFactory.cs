9bdfdbf672ffc7e24445caf7d716e7f435bb8cbd
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-29 23:18:54 +0200

    feat(api): add IValidationObjectResultFactory
    - add Create method

---
+++ Loxodonta.API/Validation/Contracts/IValidationObjectResultFactory.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Loxodonta.API.Validation.Contracts;
+
+public interface IValidationObjectResultFactory
+{
+    IActionResult Create(ModelStateDictionary modelStateDictionary);
+}
