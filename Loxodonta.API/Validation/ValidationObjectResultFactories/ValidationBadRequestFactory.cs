a2e6e25addc41fb5735614e7d823584dc9462e27
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-29 23:21:19 +0200

    feat(api): add ValidationBadRequestFactory
    - add Create method

---
+++ Loxodonta.API/Validation/ValidationObjectResultFactories/ValidationBadRequestFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Loxodonta.API.Validation.ModelStateResultFactories;
+
+public class ValidationBadRequestFactory : IValidationObjectResultFactory
+{
+    public IActionResult Create(ModelStateDictionary modelStateDictionary)
+    {
+        var problemDetails = new ValidationProblemDetails(modelStateDictionary)
+        {
+            Type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+            Title = "One or more validation errors occurred.",
+            Status = StatusCodes.Status400BadRequest
+        };
+
+        return new BadRequestObjectResult(problemDetails);
+    }
+}
