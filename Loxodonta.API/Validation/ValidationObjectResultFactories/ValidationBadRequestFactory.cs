3a046d1aa56121c8a1543e40fc4015088a6fb521
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 00:02:37 +0200

    refactor(api): update ValidationBadRequestFactory dependencies

--- Loxodonta.API/Validation/ValidationObjectResultFactories/ValidationBadRequestFactory.cs
+++ Loxodonta.API/Validation/ValidationObjectResultFactories/ValidationBadRequestFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Loxodonta.API.Validation.Contracts;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Loxodonta.API.Validation.ModelStateResultFactories;
