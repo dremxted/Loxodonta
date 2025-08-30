cc759bc3f8d943603a6725ec2c119fa38bef9952
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 16:49:09 +0200

    feat(application): add RuleBuilderOptionsExtensions
    - add MustContainUniqueValue extension method

---
+++ Loxodonta.Application/Extensions/RuleBuilderOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Loxodonta.Application.Extensions;
+
+public static class RuleBuilderOptionsExtensions
+{
+    public static IRuleBuilderOptions<T, IList<TElement>> MustContainUniqueValue<T, TElement, TKey>(
+        this IRuleBuilder<T, IList<TElement>> ruleBuilder, 
+        Func<TElement, TKey> keySelector)
+    {
+        return ruleBuilder.Must((_, list,context) =>
+        {
+            var originalCount = list.Count;
+            var distinctCount = list.DistinctBy(keySelector).Count();
+
+            if(originalCount != distinctCount)
+            {
+
+                return false;
+            }
+            return true;
+        });
+    }
+}
