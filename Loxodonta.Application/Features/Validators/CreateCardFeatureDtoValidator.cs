527f830e6eb07ea425cf8922a884b0017d85bcee
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 17:18:05 +0200

    feat(application): add CreateCardFeatureDtoValidator

---
+++ Loxodonta.Application/Features/Validators/CreateCardFeatureDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Loxodonta.Application.Features.Validators;
+
+public class CreateCardFeatureDtoValidator : AbstractValidator<CreateCardFeatureDto>
+{
+    public CreateCardFeatureDtoValidator()
+    {
+        RuleFor(feature => feature.Name)
+            .MaximumLength(255)
+            .WithMessage("Length of the value is greater than 255.");
+
+        RuleFor(feature => feature.Value)
+            .MaximumLength(255)
+            .WithMessage("Length of the value is greater than 255.");
+    }
+}
