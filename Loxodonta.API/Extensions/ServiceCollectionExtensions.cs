d371cf1d0c5e60d03ef71857b95e830cac701c66
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 17:44:16 +0200

    feat(api): add UpdateCardDtoValidator ValidationFilter to DI Container

--- Loxodonta.API/Extensions/ServiceCollectionExtensions.cs
+++ Loxodonta.API/Extensions/ServiceCollectionExtensions.cs
@@ -11,5 +11,6 @@ public static class ServiceCollectionExtensions
     {
         services.AddScoped<IValidationObjectResultFactory, ValidationBadRequestFactory>();
         services.AddScoped<ValidationFilter<CreateCardDto>>();
+        services.AddScoped<ValidationFilter<UpdateCardDto>>();
     }
 }
