834bdb2ad113cc4b888edcf2b144387a15132fdf
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 17:18:19 +0200

    feat(application): add CreateCardDtoValidator

---
+++ Loxodonta.Application/Cards/Validators/CreateCardDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Loxodonta.Application.Extensions;
+
+namespace Loxodonta.Application.Cards.Validators;
+
+public class CreateCardDtoValidator : AbstractValidator<CreateCardDto>
+{
+    public CreateCardDtoValidator()
+    {
+        RuleFor(card => card.Features)
+            .MustContainUniqueValue(feature => feature.Order)
+            .WithMessage("Order values are not unique.");
+    }
+}
