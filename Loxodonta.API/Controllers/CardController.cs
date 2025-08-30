4c67174bf0a1feab5bc621d05bd4b078ed4dd38f
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-08-30 19:12:11 +0200

    feat(api) update CardController
    - add CreateAsync location header support
    - add GetAllAsync endpoint
    - add FindAsync endpoint
    - add UpdateAsync endpoint
    - add DeleteAsync enpoint

--- Loxodonta.API/Controllers/CardController.cs
+++ Loxodonta.API/Controllers/CardController.cs
@@ -1,6 +1,6 @@
-﻿using Loxodonta.Application.Cards;
+﻿using Loxodonta.API.Validation.Filters;
+using Loxodonta.Application.Cards;
 using Loxodonta.Application.Contracts;
-using Loxodonta.Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Loxodonta.API.Controllers;
@@ -10,11 +10,47 @@ namespace Loxodonta.API.Controllers;
 public class CardController(ICardService cardService) : ControllerBase
 {
     [HttpPost]
+    [ServiceFilter(typeof(ValidationFilter<CreateCardDto>))]
     public async Task<IActionResult> CreateAsync(CreateCardDto createCardDto)
     {
         var result = await cardService.CreateAsync(createCardDto);
-        return result.Match<CardDto,IActionResult>(
-            card => Created("",card),
+        return result.Match<IActionResult>(
+            (card) =>
+            {
+                var url = Url.Link(nameof(FindAsync), new { id = card.Id });
+                return Created(url, card);
+            },
             BadRequest);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await cardService.GetAllAsync();
+        var val = result.Value.ToList();
+
+        return result.Match<IActionResult>(Ok, _ => NoContent());
+    }
+
+    [HttpGet("{id}", Name = nameof(FindAsync))]
+    public async Task<IActionResult> FindAsync(Guid id)
+    {
+        var result = await cardService.FindAsync(id);
+        return result.Match<IActionResult>(Ok, NotFound);
+    }
+
+    [HttpPut("{id}")]
+    [ServiceFilter(typeof(ValidationFilter<UpdateCardDto>))]
+    public async Task<IActionResult> UpdateAsync(Guid id, UpdateCardDto updateCardDto)
+    {
+        var result = await cardService.UpdateAsync(id, updateCardDto);
+        return result.Match<IActionResult>(Ok, NotFound);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        var result = await cardService.DeleteAsync(id);
+        return result.Match<IActionResult>(NoContent, NotFound);
     }
 }
