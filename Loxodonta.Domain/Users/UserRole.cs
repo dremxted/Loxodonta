029693e3767db729af2f3f7c766b4ecca3138299
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-07 22:33:15 +0200

    feat(domain): add UserRole

---
+++ Loxodonta.Domain/Users/UserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Loxodonta.Domain.Users;
+
+public class UserRole : IdentityUserRole<Guid>
+{
+    public User? User { get; private set; }
+    public Role? Role { get; private set; }
+}
