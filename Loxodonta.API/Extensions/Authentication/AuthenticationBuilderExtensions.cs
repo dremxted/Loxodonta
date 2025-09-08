4cfda85c95155abacca806305db160292bca2adc
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-08 23:54:43 +0200

    feat(api): add AuthenticationBuilderExtensions

---
+++ Loxodonta.API/Extensions/Authentication/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Loxodonta.Application.Common;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Loxodonta.API.Extensions.Authentication;
+
+public static class AuthenticationBuilderExtensions
+{
+    public static AuthenticationBuilder AddJwtBearerConfigured(
+        this AuthenticationBuilder authBuilder,
+        IConfigurationSection configurationSection)
+    {
+        if (configurationSection is null || !configurationSection.Exists())
+        {
+            throw new ArgumentNullException(
+                nameof(configurationSection),
+                "JWT configuration section not found or is empty");
+        }
+
+        var jwtConfiguration = configurationSection
+            .Get<JwtConfiguration>()!;
+
+        return authBuilder.AddJwtBearer(jwtOptions =>
+        {
+            jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidIssuer = jwtConfiguration.Issuer,
+                ValidAudience = jwtConfiguration.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtConfiguration.Key!)),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true
+            };
+        });
+    }
+}
