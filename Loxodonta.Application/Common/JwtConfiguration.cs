e1bc2a1bd1ae1cbd0aa796ebc7dee87465d1595d
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-08 23:52:59 +0200

    feat(application): add JwtConfiguration

---
+++ Loxodonta.Application/Common/JwtConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Loxodonta.Application.Common;
+
+/// <summary>
+/// JwtConfiguration stores data of the Configuration's JWT related section.
+/// </summary>
+public record JwtConfiguration
+{
+    public required string Key { get; init; }
+    public required string Issuer { get; init; }
+    public required string Audience { get; init; }
+}
