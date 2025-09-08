3e55cef86a93d8e8b6cf6080bdbc3383936b435f
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-08 23:54:28 +0200

    feat(api): add AuthenticationOptionsExtensions

---
+++ Loxodonta.API/Extensions/Authentication/AuthenticationOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace Loxodonta.API.Extensions.Authentication;
+
+public static class AuthenticationOptionsExtensions
+{
+    public static void UseDefaults(AuthenticationOptions authOptions)
+    {
+        authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        authOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+}
