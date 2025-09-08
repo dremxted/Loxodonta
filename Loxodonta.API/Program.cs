fc9f5a94c49decaa403f1b0379be4faf22ff8e4d
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-09 00:02:46 +0200

    feat(api): update Program add JWT authentication support clear comments

--- Loxodonta.API/Program.cs
+++ Loxodonta.API/Program.cs
@@ -1,22 +1,27 @@
 using Loxodonta.API.Extensions;
+using Loxodonta.API.Extensions.Authentication;
+using Loxodonta.Application.Common;
 using Loxodonta.Application.Extensions;
 using Loxodonta.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
+var jwtSection = config.GetSection(nameof(JwtConfiguration));
 
 // Add services to the container.
-builder.Services.AddValidationFilters();
+builder.Services.Configure<JwtConfiguration>(jwtSection);
+builder.Services.AddAuthentication(AuthenticationOptionsExtensions.UseDefaults)
+    .AddJwtBearerConfigured(jwtSection);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApplicationDbContext(config);
-
+builder.Services.AddInfrastructureDbContext(config);
 builder.Services.AddInfrastructureRepositories();
 builder.Services.AddApplicationServices();
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddValidationFilters();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -28,6 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
