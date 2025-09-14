de4acc31afd4a1655abae8843c1780e64bd5e6e3
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-14 13:22:05 +0200

    feat(infrastructure): add ApplicationDbContext RefreshToken DbSet

--- Loxodonta.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Loxodonta.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -10,10 +10,25 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     internal DbSet<Card> Cards { get; set; }
     internal DbSet<Feature> Features { get; set; }
+    internal DbSet<RefreshToken> RefreshTokens { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
+        //RefreshToken
+        modelBuilder.Entity<RefreshToken>()
+            .HasKey(rt => rt.Id);
+
+        modelBuilder.Entity<RefreshToken>()
+            .HasIndex(rt => rt.Token)
+            .IsUnique();
+
+        modelBuilder.Entity<RefreshToken>()
+            .HasOne(rt => rt.User)
+            .WithMany(u => u.RefreshTokens)
+            .HasForeignKey(rt => rt.UserId);
+
+        //User Role
         modelBuilder.Entity<UserRole>()
             .HasOne(ur => ur.User)
             .WithMany(u => u.UserRoles)
@@ -24,15 +39,17 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             .WithMany(r => r.UserRoles)
             .HasForeignKey(ur => ur.RoleId);
 
+        //Card
         modelBuilder.Entity<Card>()
             .HasKey(card => card.Id);
-
-        modelBuilder.Entity<Feature>()
-            .HasKey(feature => feature.Id);
 
         modelBuilder.Entity<Card>()
             .HasMany(card => card.Features)
             .WithOne(feature => feature.Card)
             .HasForeignKey(feature => feature.CardId);
+        
+        //Feature
+        modelBuilder.Entity<Feature>()
+            .HasKey(feature => feature.Id);
     }
 }
