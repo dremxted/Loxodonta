e97ff8b10ab5daa2e21fec324ad36308903c6a88
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-14 13:19:25 +0200

    feat(domain): add RefreshToken

---
+++ Loxodonta.Domain/Users/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Loxodonta.Domain.Users;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Token { get; set; } = null!;
+    public User User { get; set; } = null!;
+}
