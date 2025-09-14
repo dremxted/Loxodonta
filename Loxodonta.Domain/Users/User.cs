b8289f84f7ed8aea873586e9e8d677d82c3a6df2
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-14 13:20:16 +0200

    feat(domain): add User RefreshTokens navigation property

--- Loxodonta.Domain/Users/User.cs
+++ Loxodonta.Domain/Users/User.cs
@@ -5,4 +5,5 @@ namespace Loxodonta.Domain.Users;
 public class User : IdentityUser<Guid>
 {
     public List<UserRole> UserRoles { get; private set; } = new();
+    public List<RefreshToken> RefreshTokens { get; private set; } = new();
 }
