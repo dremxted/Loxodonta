708509e5d21d2fa979a1de6d0fce8541483c29fc
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:10:24 +0200

    feat(application): add RegisterRequestDto

---
+++ Loxodonta.Application/Users/Authentication/Dtos/RegisterRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Loxodonta.Application.Users.Authentication.Dtos;
+
+public record RegisterRequestDto
+{
+    public string UserName { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
