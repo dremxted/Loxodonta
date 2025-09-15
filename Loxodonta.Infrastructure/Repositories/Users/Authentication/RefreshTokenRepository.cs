94fcf6d5a183f88a368d247837fa32b90be2dc2b
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 22:27:04 +0200

    feat(infrastructure): add RefreshTokenRepository FindByValueAsync method

--- Loxodonta.Infrastructure/Repositories/Users/Authentication/RefreshTokenRepository.cs
+++ Loxodonta.Infrastructure/Repositories/Users/Authentication/RefreshTokenRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Loxodonta.Domain.Contracts;
 using Loxodonta.Domain.Users;
 using Loxodonta.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Loxodonta.Infrastructure.Repositories.Users.Authentication;
 
@@ -9,6 +10,14 @@ public class RefreshTokenRepository(ApplicationDbContext dbContext) : IRefreshTo
     public async Task<RefreshToken> CreateAsync(RefreshToken refreshToken)
     {
         await dbContext.RefreshTokens.AddAsync(refreshToken);
+        return refreshToken;
+    }
+
+    public async Task<RefreshToken?> FindByValueAsync(string token)
+    {
+        var refreshToken = await dbContext.RefreshTokens
+            .Include(t => t.User)
+            .FirstOrDefaultAsync(t => t.Token == token);
         return refreshToken;
     }
 
