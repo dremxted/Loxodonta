736c9a06c39280f23aea35902c7ba3ba9623835b
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:09:47 +0200

    feat(applicaton): add LoginSuccessDto

---
+++ Loxodonta.Application/Users/Authentication/Dtos/LoginSuccessDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Loxodonta.Application.Users.Authentication.Dtos;
+
+public record LoginSuccessDto
+{
+    public required Guid UserId { get; init; }
+    public required string AccessToken { get; init; }
+    public required int ExpiresIn { get; init; }
+    public string Username { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+    public string? RefreshToken { get; init; }
+}
