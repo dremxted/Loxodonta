eaddf326554ed20f7d6d60e742fcd3993d9de4e4
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:11:54 +0200

    feat(application): add AuthenticationErrors

---
+++ Loxodonta.Application/Users/Authentication/Errors/AuthenticationErrors.cs
@@ -0,0 +1,18 @@
+﻿using Loxodonta.Common;
+
+namespace Loxodonta.Application.Users.Authentication.Errors;
+
+public static class AuthenticationErrors
+{
+    public static Error InvalidCredentials() =>
+        Error.Failure("Users.User", "Invalid credentials provided.");
+    public static Error AccountLockedOut() =>
+        Error.Failure("Users.User", "Account is locked out.");
+    public static Error EmailNotConfirmed() =>
+        Error.Failure("Users.User", "Email is not confirmed.");
+    public static Error RequiresTwoFactor() =>
+        Error.Failure("Users.User", "Two-factor authentication is required.");
+    public static Error Failure(params object[] args) =>
+        Error.Failure("Users.User", "Sign-in attempt failed.", args);
+
+}
