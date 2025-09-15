086a3dbf2bf710a825cb5091aae326b345a29a4b
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:00:57 +0200

    feat(application): add IJwtProvider

---
+++ Loxodonta.Application/Contracts/Users/Authentication/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using Loxodonta.Domain.Users;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Loxodonta.Application.Contracts.Users.Jwt;
+
+public interface IJwtProvider
+{
+    SecurityToken CreateToken(User user);
+    string CreateRefreshToken();
+}
