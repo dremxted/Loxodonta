df7470d26986d42708f1c01303a41295d261a492
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:34:22 +0200

    feat(api): add AuthenticationController

---
+++ Loxodonta.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,24 @@
+﻿using Loxodonta.Application.Contracts.Users.Authentication;
+using Loxodonta.Application.Users.Authentication.Dtos;
+using Loxodonta.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loxodonta.API.Controllers;
+
+[ApiController]
+public class AuthenticationController(IUserAuthenticationService authService) : ControllerBase
+{
+    [HttpPost("api/login")]
+    public async Task<IActionResult> LoginUser(LoginRequestDto loginRequest)
+    {
+        Result<LoginSuccessDto> result = await authService.LoginUser(loginRequest);
+        return result.Match<IActionResult>(Ok, BadRequest);
+    }
+
+    [HttpPost("api/register")]
+    public async Task<IActionResult> RegisterUser(RegisterRequestDto registerRequest)
+    {
+        Result<RegisterSuccessDto> result = await authService.RegisterUser(registerRequest);
+        return result.Match<IActionResult>(Created, BadRequest);
+    }
+}
