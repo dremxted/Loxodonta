d9caab7562a2a34d99c93d201ec87cf1079738d8
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 20:33:44 +0200

    feat(infrastructure): add Create_RefreshTokens

--- Loxodonta.Infrastructure/Migrations/20250915183315_Create_RefreshTokens.Designer.cs
+++ Loxodonta.Infrastructure/Migrations/20250915183315_Create_RefreshTokens.Designer.cs
@@ -4,6 +4,7 @@ using Loxodonta.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Loxodonta.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250915183315_Create_RefreshTokens")]
+    partial class Create_RefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
