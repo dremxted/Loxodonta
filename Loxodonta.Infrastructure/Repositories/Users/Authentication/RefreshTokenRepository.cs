2b847ad8ee5cd011c5edbcdd3010ff576e5df39d
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-15 19:24:47 +0200

    feat(infrastructure): add RefreshTokenRepository.cs

---
+++ Loxodonta.Infrastructure/Repositories/Users/Authentication/RefreshTokenRepository.cs
@@ -0,0 +1,19 @@
+﻿using Loxodonta.Domain.Contracts;
+using Loxodonta.Domain.Users;
+using Loxodonta.Infrastructure.Persistence;
+
+namespace Loxodonta.Infrastructure.Repositories.Users.Authentication;
+
+public class RefreshTokenRepository(ApplicationDbContext dbContext) : IRefreshTokenRepository
+{
+    public async Task<RefreshToken> CreateAsync(RefreshToken refreshToken)
+    {
+        await dbContext.RefreshTokens.AddAsync(refreshToken);
+        return refreshToken;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await dbContext.SaveChangesAsync();
+    }
+}
