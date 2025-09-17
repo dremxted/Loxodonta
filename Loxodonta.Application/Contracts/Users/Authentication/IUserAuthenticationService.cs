592f6775dca71f395f092d4cf69b632b6868514a
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-17 22:19:59 +0200

    feat(application): update IUserAuthenticationService add RefreshTokenAsync

--- Loxodonta.Application/Contracts/Users/Authentication/IUserAuthenticationService.cs
+++ Loxodonta.Application/Contracts/Users/Authentication/IUserAuthenticationService.cs
@@ -7,4 +7,5 @@ public interface IUserAuthenticationService
 {
     Task<Result<LoginSuccessDto>> LoginUser(LoginRequestDto signInUserDto);
     Task<Result<RegisterSuccessDto>> RegisterUser(RegisterRequestDto registerRequestDto);
+    Task<Result<RefreshTokenDto>> RefreshTokenAsync(string refreshToken);
 }
