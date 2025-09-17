7f3bc61e11767ac2e238ce684c6a1f178123ab61
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-17 22:23:42 +0200

    refactor(domain): fix IRefreshTokenRepository CS1002

--- Loxodonta.Domain/Contracts/IRefreshTokenRepository.cs
+++ Loxodonta.Domain/Contracts/IRefreshTokenRepository.cs
@@ -5,6 +5,6 @@ namespace Loxodonta.Domain.Contracts;
 public interface IRefreshTokenRepository
 {
     Task<RefreshToken> CreateAsync(RefreshToken refreshToken);
-    Task<RefreshToken?> FindByValueAsync(string token)
+    Task<RefreshToken?> FindByValueAsync(string token);
     Task SaveChangesAsync();
 }
