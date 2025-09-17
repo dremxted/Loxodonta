1b35d0383b2d5725da020e7687ff5fddda736a57
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-17 19:06:48 +0200

    feat(domain): add RefreshToken expiration date

--- Loxodonta.Domain/Users/RefreshToken.cs
+++ Loxodonta.Domain/Users/RefreshToken.cs
@@ -2,16 +2,28 @@
 
 public class RefreshToken
 {
+    public string Token { get; private set; } = null!;
     public Guid Id { get; private set; }
     public Guid UserId { get; private set; }
-    public string Token { get; private set; } = null!;
+    public DateTime ExpiresOn { get; private set; }
     public User User { get; private set; } = null!;
 
+    public bool HasExpired => DateTime.UtcNow >= ExpiresOn;
+
     private RefreshToken() { }
+
     public RefreshToken(User user, string token)
     {
         User = user;
         UserId = user.Id;
         Token = token;
+        ExpiresOn = DateTime.UtcNow.AddDays(7);
+    }
+    public RefreshToken(User user, string token, DateTime expiresOn)
+    {
+        User = user;
+        UserId = user.Id;
+        Token = token;
+        ExpiresOn = expiresOn;
     }
 }
