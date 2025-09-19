b5a8730ba17f18d83dd26aad9bc973335f55f17f
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-19 15:16:16 +0200

    feat(api): add RegisterRequestDto validation filter

--- Loxodonta.API/Controllers/AuthenticationController.cs
+++ Loxodonta.API/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using Loxodonta.Application.Contracts.Users.Authentication;
+﻿using Loxodonta.API.Validation.Filters;
+using Loxodonta.Application.Contracts.Users.Authentication;
 using Loxodonta.Application.Users.Authentication.Dtos;
 using Loxodonta.Common;
 using Microsoft.AspNetCore.Mvc;
@@ -16,9 +17,17 @@ public class AuthenticationController(IUserAuthenticationService authService) : 
     }
 
     [HttpPost("api/register")]
+    [ServiceFilter(typeof(ValidationFilter<RegisterRequestDto>))]
     public async Task<IActionResult> RegisterUser(RegisterRequestDto registerRequest)
     {
         Result<RegisterSuccessDto> result = await authService.RegisterUser(registerRequest);
         return result.Match<IActionResult>(Created, BadRequest);
+    }
+
+    [HttpPost("api/refresh-token")]
+    public async Task<IActionResult> RefreshTokenAsync(string refreshToken)
+    {
+        Result<RefreshTokenDto> result = await authService.RefreshTokenAsync(refreshToken);
+        return result.Match<IActionResult>(Ok,BadRequest);
     }
 }
