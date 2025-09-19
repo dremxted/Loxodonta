b43077d5d8a3aa9d60e862afdb9bb46e1ef87168
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-19 17:01:43 +0200

    feat(infrastructure): add IdentityExtensions
    - add AddPasswordOptions
    - add AddIdentityConfigured

---
+++ Loxodonta.Infrastructure/Extensions/IdentityExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Loxodonta.Domain.Users;
+using Loxodonta.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Loxodonta.Infrastructure.Extensions;
+
+public static class IdentityExtensions
+{
+    public static void AddPasswordOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var passwordOptionsSection = configuration.GetSection(nameof(PasswordOptions));
+        services.Configure<PasswordOptions>(passwordOptionsSection);
+    }
+
+    public static void AddIdentityConfigured(this IServiceCollection services, IConfiguration configuration)
+    {
+        var passwordSection = configuration.GetSection(nameof(PasswordOptions));
+        var passwordOptions = passwordSection.Get<PasswordOptions>();
+
+        if(passwordOptions is null)
+        {
+            throw new NullReferenceException(nameof(passwordOptions));
+        }
+
+        services.AddIdentity<User,Role>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+            options.Password = passwordOptions;
+        }).AddEntityFrameworkStores<ApplicationDbContext>();
+    }
+}
