ceacb7d7192924dafa7752ed6511af0903d9a154
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-19 12:33:53 +0200

    feat(application): add RegisterRequestValidator

---
+++ Loxodonta.Application/Users/Authentication/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Loxodonta.Application.Users.Authentication.Dtos;
+
+namespace Loxodonta.Application.Users.Authentication.Validators;
+
+internal class RegisterRequestValidator : AbstractValidator<RegisterRequestDto>
+{
+    internal RegisterRequestValidator()
+    {
+        RuleFor(r => r.Email)
+            .EmailAddress();
+    }
+}
