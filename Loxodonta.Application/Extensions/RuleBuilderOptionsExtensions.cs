d65941c4ff79c89bc2456c064be0df56db335df5
Valerii Okhmaniuk <valerii.okhmaniuk@gmail.com>
2025-09-20 17:29:15 +0200

    feat(application): add password-related IRuleBuilderOptions extensions

--- Loxodonta.Application/Extensions/RuleBuilderOptionsExtensions.cs
+++ Loxodonta.Application/Extensions/RuleBuilderOptionsExtensions.cs
@@ -21,4 +21,58 @@ public static class RuleBuilderOptionsExtensions
             return true;
         });
     }
+
+    public static IRuleBuilderOptions<T,string> RequireLowercase<T>(
+        this IRuleBuilder<T,string> ruleBuilder)
+    {
+        return ruleBuilder.Must(prop =>
+        {
+            return prop.Any(characher => char.IsLower(characher));
+        });
+    }
+
+    public static IRuleBuilderOptions<T, string> RequireUppercase<T>(
+        this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.Must(prop =>
+        {
+            return prop.Any(characher => char.IsUpper(characher));
+        });
+    }
+
+    public static IRuleBuilderOptions<T, string> RequireDigit<T>(
+    this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.Must(prop =>
+        {
+            return prop.Any(characher => char.IsNumber(characher));
+        });
+    }
+
+    public static IRuleBuilderOptions<T, string> RequireNonAlphanumeric<T>(
+    this IRuleBuilder<T, string> ruleBuilder)
+    {
+        return ruleBuilder.Must(prop =>
+        {
+            string nonAlphanumericProp = string.Concat(
+                Array.FindAll(prop.ToCharArray(),
+                char.IsLetterOrDigit));
+
+            if(nonAlphanumericProp == prop)
+            {
+                return false;
+            }
+            return true;
+        });
+    }
+
+    public static IRuleBuilderOptions<T, string> RequiredUniqueChars<T>(
+    this IRuleBuilder<T, string> ruleBuilder, int requiredUniqueCharsNumber)
+    {
+        return ruleBuilder.Must(prop =>
+        {
+            IEnumerable<char> uniqueCharProp = prop.Distinct();
+            return uniqueCharProp.Count() >= requiredUniqueCharsNumber;
+        });
+    }
 }
